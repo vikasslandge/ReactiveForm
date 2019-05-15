cd92efc05fa8e1a19621c7fe89e3582efdac9ea6
Vikas Landge <vikasslandge@gmail.com>
2019-05-15 17:40:45 +0530

    Reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormGroup,FormControl, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ReactiveForm';
+
+  RegistrationForm = new FormGroup({
+    name: new FormControl("",[Validators.required]),
+    email: new FormControl("",[Validators.required]),
+    address: new FormGroup({
+      street: new FormControl("",[Validators.required]),
+      city: new FormControl("",[Validators.required]),
+    }),
+    phoneNo: new FormControl("",[Validators.required]),
+  });
+
+  OnSubmit(){
+    console.log(this.RegistrationForm.value);
+  }
 }
